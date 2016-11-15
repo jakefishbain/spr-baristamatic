936a15c482dff8720cb1001e5e0ead0c4d977473
Jake Fishbain <jakefishbain@gmail.com>
2016-11-15 17:38:48 -0600

    build runner back up

--- runner.rb
+++ runner.rb
@@ -1,19 +1,17 @@
 require_relative 'baristamatic'
 
 bari = Baristamatic.new
-# bari.run
 
 response = ''
-    until response == 'q' || response == 'Q'
-      if response == 'r' || response == 'R'
-        bari.restock
-      elsif (1..6).include?(response.to_i)
-        bari.order(response)
-      elsif response != ''
-        puts "Invalid Selection: #{response}"
-      end
-    bari.display_inv
-    bari.display_menu
-    response = gets.chomp
-    end
-
+until response == 'q'
+  if response == 'r'
+    bari.restock
+  elsif (1..6).include?(response.to_i)
+    bari.order(response)
+  elsif response != ''
+    puts "Invalid Selection: #{response}"
+  end
+  bari.display_inv
+  bari.display_menu
+  response = gets.chomp.downcase
+end
