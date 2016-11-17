4200c8b5c0aa688dbdfca2e319fc1bd1a024b60f
Jake Fishbain <jakefishbain@gmail.com>
2016-11-17 07:53:41 -0600

    pass test, all inv is full

--- spec/baristamatic_spec.rb
+++ spec/baristamatic_spec.rb
@@ -3,7 +3,15 @@ require_relative '../baristamatic'
 describe Baristamatic do 
   let(:bari) { Baristamatic.new }
 
-  it 'has an inventory that is full' do
+  it 'has an inventory' do
     expect(bari.inventory.count).to eq 9
   end
+
+  it 'has full inventory upon startup' do
+    full = ''
+    bari.inventory.each do |ingredient|
+      expect(ingredient[1][:inventory]).to eq 10
+    end
+  end
+
 end
