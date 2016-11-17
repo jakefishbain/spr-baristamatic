6b7aabd56fc28716cfee9e37dd4e89629c126026
Jake Fishbain <jakefishbain@gmail.com>
2016-11-17 07:43:55 -0600

    pass first test - has inv

--- spec/baristamatic_spec.rb
+++ spec/baristamatic_spec.rb
@@ -1,7 +1,9 @@
-require_relative '_/baristamatic'
+require_relative '../baristamatic'
 
 describe Baristamatic do 
+  let(:bari) { Baristamatic.new }
 
-
-
+  it 'has an inventory that is full' do
+    expect(bari.inventory.count).to eq 9
+  end
 end
